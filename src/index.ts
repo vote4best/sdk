cb15619ddd229ed3a5ac67ace829043caced03db
Tim Pechersky <tim.pechersky@gmail.com>
2023-11-24 09:39:12 +0000

    improvements, cicd, docs

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+/**
+ * This file contains functions related to the SDK for the rankify.it .
+ * It includes functions for interacting with smart contracts on different chains,
+ * retrieving game state and proposal scores, and creating games.
+ */
 import { LibCoinVending } from "rankify-contracts/types/hardhat-diamond-abi/HardhatDiamondABI.sol/RankifyDiamondInstance";
 import {
   RankToken,
@@ -6,7 +11,7 @@ import {
 } from "rankify-contracts/types";
 import { BigNumberish, Bytes, BytesLike, ethers, Wallet } from "ethers";
 import { JsonFragment } from "@ethersproject/abi";
-
+import _ from "lodash";
 export type SupportedChains =
   | "anvil"
   | "localhost"
@@ -28,6 +33,12 @@ export const chainIds = {
   anvil: 97113,
 };
 
+/**
+ * Retrieves the artifact for a given chain.
+ * @param chain The chain identifier.
+ * @returns An object containing the ABI and address of the artifact.
+ * @throws Error if the contract deployment is not found.
+ */
 export const getArtifact = (
   chain: string
 ): { abi: JsonFragment[]; address: string } => {
@@ -38,6 +49,12 @@ export const getArtifact = (
   return artifact;
 };
 
+/**
+ * Retrieves the rank artifact for a given chain.
+ * @param chain The chain identifier.
+ * @returns The rank artifact containing the ABI and address.
+ * @throws Error if the contract deployment is not found.
+ */
 const getRankArtifact = (
   chain: string
 ): { abi: JsonFragment[]; address: string } => {
@@ -50,6 +67,12 @@ const getRankArtifact = (
   return artifact;
 };
 
+/**
+ * Retrieves the Rankify artifact for the specified chain.
+ * @param chain The chain identifier.
+ * @returns The Rankify artifact containing the ABI and address.
+ * @throws Error if the contract deployment is not found.
+ */
 const getRankifyArtifact = (
   chain: string
 ): { abi: JsonFragment[]; address: string } => {
@@ -62,12 +85,17 @@ const getRankifyArtifact = (
   return artifact;
 };
 
+/**
+ * Retrieves the contract instance for the specified chain using the provided provider.
+ * @param chain The supported chain for the contract.
+ * @param provider The Web3Provider or Signer instance used for interacting with the blockchain.
+ * @returns The contract instance.
+ */
 export const getContract = (
   chain: SupportedChains,
   provider: ethers.providers.Web3Provider | ethers.Signer
 ) => {
   const artifact = getArtifact(chain);
-  console.log("artifact address", artifact.address);
   return new ethers.Contract(
     artifact.address,
     artifact.abi,
@@ -75,6 +103,12 @@ export const getContract = (
   ) as RankifyDiamondInstance;
 };
 
+/**
+ * Retrieves the RankToken contract instance for the specified chain and provider.
+ * @param chain The supported chain for the contract.
+ * @param provider The Web3Provider or Signer instance used to interact with the blockchain.
+ * @returns The RankToken contract instance.
+ */
 export const getRankTokenContract = (
   chain: SupportedChains,
   provider: ethers.providers.Web3Provider | ethers.Signer
@@ -88,6 +122,13 @@ export const getRankTokenContract = (
   return contract;
 };
 
+/**
+ * Retrieves the Rankify token contract instance.
+ *
+ * @param chain - The supported blockchain network.
+ * @param provider - The Web3 provider or signer instance.
+ * @returns The Rankify token contract instance.
+ */
 export const getRankifyTokenContract = (
   chain: SupportedChains,
   provider: ethers.providers.Web3Provider | ethers.Signer
@@ -101,12 +142,43 @@ export const getRankifyTokenContract = (
   return contract;
 };
 
+/**
+ * Ethers.js returns a proxy object of mixed arrays and objects. This proxy breaks when using hardcopy.
+ * Im still in progress of solving this issue. For now, this function is a workaround that converts the proxy to a deep copy.
+ * it might produce unexpected results still.
+ * @param object The object to convert.
+ * @returns The converted object.
+ */
+const deepArrayToObject = <T>(object: T) => {
+  console.log("deepArrayToObject", typeof object);
+  let result = Array.isArray(object) ? [] : {};
+  Object.keys(object).forEach((key) => {
+    if (typeof object[key] === "string")
+      result[key] = (" " + object[key]).slice(1);
+    else if (typeof object[key] === "number") result[key] = Number(object[key]);
+    else if (typeof object[key] === "boolean")
+      result[key] = Boolean(object[key]);
+    else if (object[key] === null) result[key] = null;
+    else if (object[key] === undefined) result[key] = undefined;
+    else if (object[key]?._isBigNumber)
+      result[key] = ethers.BigNumber.from(
+        (" " + object[key].toString()).slice(1)
+      );
+    else result[key] = deepArrayToObject(object[key]);
+  });
+  return result as T;
+};
+
 export const getContractState = async (
   chain: SupportedChains,
   provider: ethers.providers.Web3Provider
 ) => {
   const contract = getContract(chain, provider);
-  return contract.getContractState();
+  const cs = await contract
+    .getContractState()
+    .then((x) => deepArrayToObject(x));
+  console.log("cscscs", cs);
+  return cs;
 };
 
 export const getPlayersGame =
@@ -126,8 +198,7 @@ export const getRankTokenURI = async (
     artifact.abi,
     provider
   ) as RankToken;
-  const retval = await contract.contractURI();
-  return retval;
+  return contract.contractURI().then((x) => deepArrayToObject(x));
 };
 
 export const getRankTokenBalance =
@@ -139,9 +210,20 @@ export const getRankTokenBalance =
       artifact.abi,
       provider
     ) as RankToken;
-    return await contract.balanceOf(account, tokenId);
+    return contract.balanceOf(account, tokenId);
   };
 
+/**
+ * Retrieves the list of proposal scores for a specific game.
+ *
+ * @param chain - The supported chain.
+ * @param provider - The Web3 provider.
+ * @param gameId - The ID of the game.
+ * @param from - Optional. The starting turn number. Defaults to 1.
+ * @param to - Optional. The ending turn number. Defaults to the latest turn number of the game.
+ * @returns A Promise that resolves to the list of proposal scores.
+ * @throws An error if the gameId is not set.
+ */
 export const getProposalScoresList = async (
   chain: SupportedChains,
   provider: ethers.providers.Web3Provider,
@@ -149,14 +231,24 @@ export const getProposalScoresList = async (
   from?: number,
   to?: number
 ) => {
+  if (!gameId) throw new Error("gameId not set");
   const contract = getContract(chain, provider);
   const _from = from ?? 1;
   const _to = to ?? (await contract.getTurn(gameId)).toNumber();
   console.log("getProposalScore result", gameId, _from, _to);
   const proposalScoreFilter = contract.filters.ProposalScore(gameId);
-  return contract.queryFilter(proposalScoreFilter, 0, "latest");
+  const res = await contract.queryFilter(proposalScoreFilter, 0, "latest");
+  console.log("resres", res);
+  return deepArrayToObject(res);
 };
 
+/**
+ * Retrieves the current turn of a game.
+ * @param chain - The supported blockchain network.
+ * @param provider - The Web3 provider.
+ * @param gameId - The ID of the game.
+ * @returns A Promise that resolves to the current turn of the game.
+ */
 export const getCurrentTurn = async (
   chain: SupportedChains,
   provider: ethers.providers.Web3Provider,
@@ -167,6 +259,13 @@ export const getCurrentTurn = async (
   return currentTurn;
 };
 
+/**
+ * Retrieves the game state for a specific game.
+ * @param chain - The supported blockchain network.
+ * @param provider - The Web3 provider.
+ * @param gameId - The ID of the game.
+ * @returns A promise that resolves to an object containing the game state.
+ */
 export const getGameState = async (
   chain: SupportedChains,
   provider: ethers.providers.Web3Provider,
@@ -240,6 +339,14 @@ export const getGameState = async (
   });
 };
 
+/**
+ * Approves tokens if needed.
+ *
+ * @param value - The value of tokens to approve.
+ * @param chain - The supported chain.
+ * @param signer - The JSON-RPC signer.
+ * @returns A promise that resolves to the transaction receipt or 0 if no tokens need to be approved.
+ */
 const approveTokensIfNeeded = async (
   value: BigNumberish,
   chain: SupportedChains,
@@ -276,6 +383,12 @@ export const createGame =
     );
   };
 
+/**
+ * Joins a game on the specified chain using the provided signer.
+ * @param chain - The supported chain on which the game is being joined.
+ * @param signer - The JSON-RPC signer used for signing the transaction.
+ * @returns A promise that resolves to the transaction receipt once the join transaction is confirmed.
+ */
 export const joinGame =
   (chain: SupportedChains, signer: ethers.providers.JsonRpcSigner) =>
   async (gameId: string) => {
@@ -291,6 +404,12 @@ export const joinGame =
       .then((tx) => tx.wait(1));
   };
 
+/**
+ * Starts a game on the specified chain using the provided signer.
+ * @param chain The supported chain on which the game will be started.
+ * @param signer The JSON-RPC signer used to sign the transaction.
+ * @returns A promise that resolves to the result of the startGame transaction.
+ */
 export const startGame =
   (chain: SupportedChains, signer: ethers.providers.JsonRpcSigner) =>
   async (gameId: string) => {
@@ -298,6 +417,13 @@ export const startGame =
     return await contract.startGame(gameId);
   };
 
+/**
+ * Cancels a game.
+ *
+ * @param chain - The supported blockchain network.
+ * @param signer - The JSON-RPC signer.
+ * @returns A promise that resolves to the result of cancelling the game.
+ */
 export const cancelGame =
   (chain: SupportedChains, signer: ethers.providers.JsonRpcSigner) =>
   async (gameId: string) => {
@@ -305,13 +431,12 @@ export const cancelGame =
     return await contract.cancelGame(gameId);
   };
 
-export const checkSignature =
-  (chain: SupportedChains, provider: ethers.providers.Web3Provider) =>
-  async (message: BytesLike, signature: BytesLike, account: string) => {
-    const contract = getContract(chain, provider);
-    return await contract.checkSignature(message, signature, account);
-  };
-
+/**
+ * Leaves a game.
+ * @param chain - The blockchain network.
+ * @param signer - The signer object.
+ * @returns A promise that resolves to the transaction receipt.
+ */
 export const leaveGame =
   (chain: SupportedChains, signer: ethers.providers.JsonRpcSigner) =>
   async (gameId: string) => {
@@ -319,6 +444,12 @@ export const leaveGame =
     return contract.leaveGame(gameId).then((tx) => tx.wait(1));
   };
 
+/**
+ * Opens the registration for a game on the specified chain using the provided signer.
+ * @param chain - The supported chain on which the game is being played.
+ * @param signer - The JSON-RPC signer used to interact with the blockchain.
+ * @returns A promise that resolves to the result of opening the registration.
+ */
 export const openRegistration =
   (chain: SupportedChains, signer: ethers.providers.JsonRpcSigner) =>
   async (gameId: string) => {
@@ -326,6 +457,13 @@ export const openRegistration =
     return await contract.openRegistration(gameId);
   };
 
+/**
+ * Sets the join requirements for a game on the specified chain using the provided signer.
+ *
+ * @param chain - The supported chain on which the game is being played.
+ * @param signer - The JSON-RPC signer used to interact with the contract.
+ * @returns A promise that resolves to the result of setting the join requirements.
+ */
 export const setJoinRequirements =
   (chain: SupportedChains, signer: ethers.providers.JsonRpcSigner) =>
   async (gameId: string, config: LibCoinVending.ConfigPositionStruct) => {
@@ -356,6 +494,13 @@ export async function getApiError(response: any) {
   });
 }
 
+/**
+ * Retrieves the historic turn information for a specific game and turn ID.
+ * @param chain - The supported blockchain network.
+ * @param provider - The Web3 provider.
+ * @returns The historic turn event object.
+ * @throws {ApiError} If the game or turn is not found.
+ */
 export const getHistoricTurn =
   (chain: SupportedChains, provider: ethers.providers.Web3Provider) =>
   async (gameId: BigNumberish, turnId: BigNumberish) => {
@@ -382,6 +527,16 @@ export const getHistoricTurn =
     return { ...turnEndedEvents[0] };
   };
 
+/**
+ * Retrieves the statistics of the previous turn for a specific game.
+ * If the current turn is greater than 1, it calls the `getHistoricTurn` function to retrieve the statistics of the previous turn.
+ * If the current turn is 1 or less, it returns a default object with "N/A" values.
+ *
+ * @param chain - The supported chain for the game.
+ * @param provider - The Web3 provider.
+ * @param gameId - The ID of the game.
+ * @returns The statistics of the previous turn.
+ */
 export const getPreviousTurnStats = async (
   chain: SupportedChains,
   provider: ethers.providers.Web3Provider,
@@ -402,9 +557,18 @@ export const getPreviousTurnStats = async (
   }
 };
 
+/**
+ * Retrieves the voting information for a specific game and turn.
+ * @param chain - The supported chain for the voting.
+ * @param signer - The JSON-RPC signer for the voting contract.
+ * @returns An object containing the vote events and fixed proposal arguments.
+ * @throws Error if gameId or turnId is not set.
+ */
 export const getVoting =
   (chain: SupportedChains, signer: ethers.providers.JsonRpcSigner) =>
   async (gameId: BigNumberish, turnId: BigNumberish) => {
+    if (!gameId) throw new Error("gameId not set");
+    if (!turnId) throw new Error("turnId not set");
     console.log("getVoting", gameId, turnId);
     const contract = getContract(chain, signer);
     const filterVoteEvent = contract.filters.VoteSubmitted(gameId, turnId);
@@ -421,8 +585,6 @@ export const getVoting =
       "latest"
     );
     const voteEvents = await contract.queryFilter(filterVoteEvent, 0, "latest");
-    let proposals = [];
-    let playersVoted = [];
     console.log("voteEvents", voteEvents, "proposalEvents", proposalEvents);
     const fixedProposalArgs = proposalEvents.map((event) => {
       return {
@@ -447,6 +609,14 @@ export const getVoting =
     return { voteEvents, proposalEvents: fixedProposalArgs };
   };
 
+/**
+ * Retrieves the ongoing voting for a specific game.
+ *
+ * @param {object} options - The options for retrieving the ongoing voting.
+ * @param {SupportedChains} options.chain - The supported chain for the voting.
+ * @param {ethers.providers.JsonRpcSigner} options.signer - The signer for the voting.
+ * @returns {Promise<ReturnType<typeof getVoting>>} - A promise that resolves to the ongoing voting for the specified game.
+ */
 export const getOngoingVoting =
   ({
     chain,
@@ -461,6 +631,14 @@ export const getOngoingVoting =
       .then((turn) => getVoting(chain, signer)(gameId, turn));
   };
 
+/**
+ * Retrieves the ongoing proposals for a specific game on a supported chain.
+ *
+ * @param chainName - The name of the supported chain.
+ * @param provider - The Web3Provider instance.
+ * @param gameId - The ID of the game.
+ * @returns The ongoing proposals for the specified game.
+ */
 export const getOngoingProposals = async (
   chainName: SupportedChains,
   provider: ethers.providers.Web3Provider,
@@ -473,10 +651,20 @@ export const getOngoingProposals = async (
   const filter = contract.filters.TurnEnded(gameId, currentTurn.sub(1));
   const TurnEndedEvents = await contract.queryFilter(filter, 0, "latest");
   const event = contract.interface.parseLog(TurnEndedEvents[0]);
-  console.log("getOngoingProposals", event);
-  return event.args.newProposals;
+  console.debug("aaaaaa", event);
+  const dc = deepArrayToObject(event);
+  console.debug("aaaaaa", event, dc);
+  return dc.args.newProposals;
 };
 
+/**
+ * Retrieves the registration deadline for a specific game on a supported blockchain.
+ * @param chainName - The name of the blockchain.
+ * @param provider - The Web3Provider instance.
+ * @param gameId - The ID of the game.
+ * @param timeToJoin - Optional. The additional time (in seconds) to join the game.
+ * @returns A Promise that resolves to the registration deadline timestamp.
+ */
 export const getRegistrationDeadline = async (
   chainName: SupportedChains,
   provider: ethers.providers.Web3Provider,
@@ -496,6 +684,15 @@ export const getRegistrationDeadline = async (
   );
 };
 
+/**
+ * Resolves the deadline for the current turn.
+ * If `timePerTurn` is provided, the deadline is calculated by adding `timePerTurn` to the current block timestamp.
+ * Otherwise, the deadline is obtained from the contract state and calculated by adding `timePerTurn` to the current block timestamp.
+ * @param block The current block.
+ * @param contract The RankifyDiamondInstance contract.
+ * @param timePerTurn The time duration per turn (optional).
+ * @returns The deadline for the current turn.
+ */
 const resolveTurnDeadline = async (
   block: ethers.providers.Block,
   contract: RankifyDiamondInstance,
@@ -508,12 +705,23 @@ const resolveTurnDeadline = async (
     .then((cs) => cs.TBGSEttings.timePerTurn.toNumber() + block.timestamp);
 };
 
+/**
+ * Retrieves the deadline for the current turn in a game.
+ *
+ * @param chainName - The name of the blockchain network.
+ * @param provider - The Web3Provider instance.
+ * @param gameId - The ID of the game.
+ * @param timePerTurn - Optional. The duration of each turn in seconds.
+ * @returns A Promise that resolves to the deadline for the current turn, or 0 if the turn has not started.
+ * @throws An error if the gameId is not set.
+ */
 export const getTurnDeadline = async (
   chainName: SupportedChains,
   provider: ethers.providers.Web3Provider,
   gameId: BigNumberish,
   timePerTurn?: number
 ) => {
+  if (!gameId) throw new Error("gameId not set");
   const contract = getContract(chainName, provider);
 
   return contract.getTurn(gameId).then(async (ct) => {
@@ -533,6 +741,10 @@ export const getTurnDeadline = async (
   });
 };
 
+/**
+ * Represents a player in the voting system.
+ * ToDo: methods above ^^^^ mostly can be moved to this class
+ */
 export class Player {
   EIP712name: string;
   EIP712Version: string;
@@ -540,6 +752,15 @@ export class Player {
   verifyingContract: string;
   chainId: string;
 
+  /**
+   * Creates a new instance of the Player class.
+   * @param {Object} options - The options for initializing the Player.
+   * @param {string} options.EIP712name - The name of the EIP712 standard.
+   * @param {string} options.EIP712Version - The version of the EIP712 standard.
+   * @param {string} options.verifyingContract - The address of the contract being verified.
+   * @param {ethers.providers.Web3Provider} options.provider - The Web3 provider used for interacting with the blockchain.
+   * @param {string} options.chainId - The chain ID of the blockchain network.
+   */
   constructor({
     EIP712name,
     EIP712Version,
