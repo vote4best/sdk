30ab10f60ec39569cd690d1a34620b12a32534b8
Tim Pechersky <tim.pechersky@gmail.com>
2024-04-29 23:58:14 +0800

    build process fixes

--- copyPackageFile.js
+++ copyPackageFile.js
@@ -3,13 +3,14 @@ const path = require("path");
 const stage = process.argv[2];
 const copyPackgeFile = () => {
   const packageJson = require("./package.json");
+  console.log("pv", packageJson.version);
   delete packageJson.private;
   console.log("stage is", stage);
   if (stage === "dev") {
     packageJson.dependencies["rankify-contracts"] = "file:../../contracts";
   }
   packageJson.type = "commonjs";
-  packageJson.main = '"./lib.commonjs/index.js"';
+  packageJson.main = "./lib.commonjs/index.js";
   packageJson.module = "./lib.esm/index.js";
   packageJson.exports["."] = {
     require: "./lib.commonjs/index.js",
