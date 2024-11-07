fa0a9f1b9f9cdd2d2a8d7791724baa36613ed1b3
Peersky <61459744+peersky@users.noreply.github.com>
2024-11-07 12:44:24 +0700

    configure editorconf and eslint (#35)

---
+++ eslint.config.mjs
@@ -0,0 +1,46 @@
+import promise from "eslint-plugin-promise";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+import prettier from "eslint-plugin-prettier";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  {
+    ignores: ["deployments/", "coverage/**", "docs/templates/", "**/node_modules/"],
+  },
+  ...compat.extends("eslint:recommended", "plugin:promise/recommended", "prettier"),
+  {
+    plugins: {
+      promise,
+      prettier, // Add Prettier plugin
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.mocha,
+        artifacts: "readonly",
+        contract: "readonly",
+        assert: "readonly",
+        web3: "readonly",
+      },
+      parser: tsParser,
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "prettier/prettier": "error", // Add Prettier as an ESLint rule
+    },
+    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+  },
+];
